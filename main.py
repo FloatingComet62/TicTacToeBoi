e03e8f57f024de6a90e1440b1d46ae631d5c7827
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-06-23 23:35:58 +0530

    better pong command and jishaku

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import os
 import discord
 from discord.ext import commands
 
-load_dotenv()
+if os.path.isfile(".env"):
+    load_dotenv()
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='>', intents=intents)
+bot.load_extension('jishaku')
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send(f'Pong :ping_pong: (Latency {round(bot.latency)})')
 
 bot.run(os.getenv("TOKEN"))
