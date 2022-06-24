15980a2832731035782f4e6ace68b70fc1ed426c
FloatingComet62 <shubh2007rai@gmail.com>
2022-06-24 10:38:34 +0530

    tic command

--- cogs/ai.py
+++ cogs/ai.py
@@ -5,6 +5,11 @@ import os
 class AI(commands.Cog):
     def __init__(self, client):
         self.client = client
+    
+    @commands.command()
+    async def tic(self, ctx):
+        respond = discord.Embed(title="Tic Tac Toe", description=(("⬛"*3)+"\n")*3)
+        await ctx.send(embed=respond)
 
 def setup(bot):
     bot.add_cog(AI(bot))
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pydoc import describe
 from dotenv import load_dotenv
 import os
 import discord
